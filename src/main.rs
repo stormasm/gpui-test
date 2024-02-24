6c6fa6dde15f371111e574fadaf2f31a669b80aa
Levin Uncu <levin@uncu.de>
2024-02-24 16:57:32 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+use std::time::Duration;
+
+use chrono::{DateTime, Local};
+use gpui::*;
+
+struct Wrapper {}
+impl Wrapper {
+    fn new() -> Self {
+        Self {}
+    }
+}
+impl Render for Wrapper {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let datum = cx.new_view(|_| Datum::new());
+
+        div()
+            .size_full()
+            .text_color(white())
+            .flex()
+            .flex_col()
+            .gap_3()
+            .justify_center()
+            .items_center()
+            .child(datum)
+    }
+}
+struct Datum {}
+impl Datum {
+    fn new() -> Self {
+        Self {}
+    }
+}
+impl Render for Datum {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let app_state = cx.global::<AppState>();
+        let time = app_state.global_store.read(cx).time;
+        let global_store = app_state.global_store.clone();
+
+        cx.observe(&global_store, |_, _, cx| {
+            cx.notify();
+        })
+        .detach();
+
+        div().child(format!("time: {}", time))
+    }
+}
+
+struct GlobalStore {
+    time: DateTime<Local>,
+}
+impl GlobalStore {
+    fn init(cx: &mut AppContext) -> Model<Self> {
+        let global_store = cx.new_model(|_| GlobalStore::new());
+        let a = global_store.clone();
+        cx.spawn(|mut cx| async move {
+            loop {
+                let _ = a.update(&mut cx, |global, cx| {
+                    global.time = Local::now();
+                    cx.notify();
+                });
+                cx.background_executor().timer(Duration::from_secs(1)).await;
+            }
+        })
+        .detach();
+
+        global_store
+    }
+    fn new() -> Self {
+        Self { time: Local::now() }
+    }
+}
+
+struct AppState {
+    global_store: Model<GlobalStore>,
+}
+impl Global for AppState {}
+impl AppState {
+    fn init(cx: &mut AppContext) {
+        let global_store = GlobalStore::init(cx);
+        cx.set_global(AppState::new(global_store));
+    }
+    fn new(global_store: Model<GlobalStore>) -> Self {
+        Self { global_store }
+    }
+}
+
+fn main() {
+    App::new().run(|cx| {
+        cx.activate(true);
+        AppState::init(cx);
+        cx.open_window(WindowOptions::default(), |cx| {
+            cx.new_view(|_| Wrapper::new())
+        });
+    })
+}
